f4551a3e8ebf234dc689a74747492eee25483e19
Toby Griffths <toby@cubicmushroom.co.uk>
2014-07-26 00:08:03 +0100

    Updated nsenter to use the container name passed in to script

--- nsenter.sh
+++ nsenter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Assuming sudo docker run --rm --name flexdisc_instance -i -t flexdisc
-PID=$(docker inspect --format {{.State.Pid}} flexdisc_instance)
+PID=$(docker inspect --format {{.State.Pid}} $1)
 sudo nsenter --target $PID --mount --uts --ipc --net --pid
